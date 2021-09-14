11b08c2696824ca99bc567a17938017cafaa5bd6
unknown <nhanth99@gmail.com>
2021-09-14 21:30:45 +0700

    Native commit

--- src/main/java/com/example/springdatajpa/repo/StudentRepository.java
+++ src/main/java/com/example/springdatajpa/repo/StudentRepository.java
@@ -1,2 +1,30 @@
-package com.example.springdatajpa.repo;public class StudentRepository {
+package com.example.springdatajpa.repo;
+import com.example.springdatajpa.entity.*;
+
+import org.hibernate.metamodel.model.convert.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
+    @Query(value = "SELECT * FROM student WHERE name = 'studentA'",
+            nativeQuery = true)
+    List<Student> findStudentByName();
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM student WHERE id = :id", nativeQuery = true)
+    void deleteStudent(long id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Student SET email= ?1 WHERE id = ?2", nativeQuery = true)
+    void updateStudent(String email, long id);
+
 }
--- src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
+++ src/test/java/com/example/springdatajpa/SpringDataJpaApplicationTests.java
@@ -1,6 +1,8 @@
+
 package com.example.springdatajpa;
 
-import com.example.entity.Student;
+import com.example.springdatajpa.entity.Student;
+import com.example.springdatajpa.repo.StudentRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +12,23 @@ import java.util.List;
 @SpringBootTest
 class SpringDataJpaApplicationTests {
 
+    @Autowired
+    private StudentRepository studentRepository;
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void findStudentByName(){
+        List<Student> list = studentRepository.findStudentByName();
+    }
+    @Test
+    void updateStudent(){
+        studentRepository.updateStudent("email@gmail.com",1l);
+    }
+    @Test
+    void deleteStudent(){
+        studentRepository.deleteStudent(1l);
     }
 
 
